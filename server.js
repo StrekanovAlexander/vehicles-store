662fc4aaf22d3b6fae8c92cbda05bbe3137da416
Alexander Strekanov <strekanov.alexander@gmail.com>
2024-05-17 21:38:52 +0200

    rebuilded: package.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => res.send('Vehicles Store Application'));
+app.get('/', (req, res) => res.send('Vehicles Store Application 2'));
 
 app.listen(3000);
